e9adcc1f68cef8610e692ca09d53cb235f73bde3
neilicatan <111211509+neilicatan@users.noreply.github.com>
2023-09-13 20:29:33 +0800

    Add files via upload

---
+++ Bool.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool isTrue = true;
+        bool isFalse = false;
+
+        Console.WriteLine($"isTrue: {isTrue}");
+        Console.WriteLine($"isFalse: {isFalse}");
+    }
+}
